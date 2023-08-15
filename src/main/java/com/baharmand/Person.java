b1140d221271a1fee8018c2faf1b3a6122d8ca4c
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-15 13:58:00 +0200

    Initial commit

---
+++ src/main/java/com/baharmand/Person.java
@@ -0,0 +1,9 @@
+package com.baharmand;
+
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
---
+++ src/main/java/com/baharmand/TodoItemTask.java
@@ -0,0 +1,4 @@
+package com.baharmand;
+
+public class TodoItemTask {
+}

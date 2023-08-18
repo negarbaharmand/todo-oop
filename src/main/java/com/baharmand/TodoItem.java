7608b56b2b81ce31d5ba8836ed4ad639e14854c9
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-18 15:31:08 +0200

    Complete TodoItem class and junit tests

--- src/main/java/com/baharmand/TodoItem.java
+++ src/main/java/com/baharmand/TodoItem.java
@@ -1,4 +1,83 @@
 package com.baharmand;
 
+import java.time.LocalDate;
+
 public class TodoItem {
+    private static int lastId = 0;
+    private final int id;
+    private String title;
+    private String taskDescription;
+    private LocalDate deadLine;
+    private boolean done;
+    private Person creator;
+
+    public TodoItem(String title, String description, LocalDate deadLine, Person creator) {
+        this.id = ++lastId;
+        this.title = title;
+        this.taskDescription = description;
+        this.deadLine = deadLine;
+        this.creator = creator;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be null or empty.");
+        }
+        this.title = title;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+
+    public void setDeadLine(LocalDate deadLine) {
+        if (deadLine == null) {
+            throw new IllegalArgumentException("Deadline cannot be null.");
+        }
+        this.deadLine = deadLine;
+    }
+
+
+    public Person getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Person creator) {
+        this.creator = creator;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public boolean isOverdue() {
+        return LocalDate.now().isAfter(deadLine);
+    }
+
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+
+    public String getSummary() {
+        return "id: " + id + " Title: " + title + " Description: " + taskDescription;
+    }
 }
---
+++ src/test/java/com/baharmand/TodoItemTest.java
@@ -0,0 +1,35 @@
+package com.baharmand;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoItemTest {
+    private Person person;
+
+    @BeforeEach
+    public void setup() {
+        person = new Person("John", "Doe", "john@example.com");
+    }
+
+    @Test
+    public void testIsOverdue() {
+        LocalDate pastDate = LocalDate.now().minusDays(1);
+        TodoItem overdueItem = new TodoItem("Title", "Description", pastDate, person);
+        assertTrue(overdueItem.isOverdue());
+
+        LocalDate futureDate = LocalDate.now().plusDays(1);
+        TodoItem notOverdueItem = new TodoItem("Title", "Description", futureDate, person);
+        assertFalse(notOverdueItem.isOverdue());
+    }
+    @Test
+    public void testSetTitleNotNullOrEmpty() {
+        TodoItem todoItem = new TodoItem("Title", "Description", LocalDate.now(), person);
+        assertThrows(IllegalArgumentException.class, () -> todoItem.setTitle(null));
+        assertThrows(IllegalArgumentException.class, () -> todoItem.setTitle(""));
+    }
+
+}

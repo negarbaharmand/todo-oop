ca96ebdea1a472617b303085cdf982fdf319ac42
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-20 10:55:48 +0200

    Modify Person Class and tests

--- src/main/java/com/baharmand/Person.java
+++ src/main/java/com/baharmand/Person.java
@@ -1,11 +1,14 @@
 package com.baharmand;
 
+import java.util.Objects;
+
 public class Person {
     private static int lastId = 0;
     private final int id;
     private String firstName;
     private String lastName;
     private String email;
+    private AppUser credential;
 
     public Person(String firstName, String lastName, String email) {
         this.id = ++lastId;
@@ -44,7 +47,30 @@ public class Person {
         if (email == null) throw new IllegalArgumentException("Email was null.");
         this.email = email;
     }
-    public String getSummary() {
+
+    public void setCredential(AppUser credential) {
+        this.credential = credential;
+    }
+
+    public AppUser getCredential() {
+        return credential;
+    }
+
+    @Override
+    public String toString() {
         return "id: " + id + "First Name: " + firstName + "Last Name: " + lastName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, id);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Person person = (Person) object;
+        return Objects.equals(email, person.email) && id == person.id;
     }
 }
--- src/test/java/com/baharmand/PersonTest.java
+++ src/test/java/com/baharmand/PersonTest.java
@@ -4,19 +4,27 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class PersonTest {
     private Person person1;
+    private Person person2;
 
     @BeforeEach
     public void setup() {
-      person1 = new Person("John", "Doe", "john@example.com");
+      person1 = new Person("John", "Test", "test@example.com");
+      person2 = new Person("Kell", "Teston", "Kell@gmail.con");
     }
 
     @Test
-    public void testGetSummary() {
+    public void testToString() {
         String expectedSummary = "id: " + person1.getId() + "First Name: " + person1.getFirstName() + "Last Name: " + person1.getLastName();
-        assertEquals(expectedSummary, person1.getSummary());
+        assertEquals(expectedSummary, person1.toString());
+    }
+
+    @Test
+    public void testHashCode(){
+        assertNotEquals(person2.hashCode(), person1.hashCode());
     }
 
 

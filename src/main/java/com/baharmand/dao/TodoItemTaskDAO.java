2b8077f93e5c28e581cec569093418eaa93417eb
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-30 15:48:10 +0200

    Implement TodoItemTaskDAOCollection

--- src/main/java/com/baharmand/dao/TodoItemTaskDAO.java
+++ src/main/java/com/baharmand/dao/TodoItemTaskDAO.java
@@ -1,11 +1,10 @@
 package com.baharmand.dao;
 
-import com.baharmand.model.TodoItem;
 import com.baharmand.model.TodoItemTask;
 
 import java.util.Collection;
 
-public interface TodoItemTaskDAO extends BaseDAO<TodoItem, Integer>{
+public interface TodoItemTaskDAO extends BaseDAO<TodoItemTask, Integer>{
     TodoItemTask findById(int id);
     Collection<TodoItemTask> findByAssignedStatus(boolean assigned);
     Collection<TodoItemTask> findByPersonId(int id);
--- src/main/java/com/baharmand/dao/TodoItemTaskDAOCollection.java
+++ src/main/java/com/baharmand/dao/TodoItemTaskDAOCollection.java
@@ -1,43 +1,73 @@
 package com.baharmand.dao;
 
-import com.baharmand.model.TodoItem;
 import com.baharmand.model.TodoItemTask;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
-public class TodoItemTaskDAOCollection implements TodoItemTaskDAO{
+public class TodoItemTaskDAOCollection implements TodoItemTaskDAO {
+    private Collection<TodoItemTask> todoItemTasks;
+
+    public TodoItemTaskDAOCollection() {
+        todoItemTasks = new ArrayList<>();
+    }
+
+
     @Override
-    public int hashCode() {
-        return super.hashCode();
+    public void persist(TodoItemTask todoItemTask) {
+        todoItemTasks.add(todoItemTask);
+
     }
 
     @Override
-    public void persist(TodoItem todoItem) {
+    public void remove(Integer id) {
+        TodoItemTask taskToRemove = null;
+        for (TodoItemTask task : todoItemTasks) {
+            if (task.getId() == id) {
+                taskToRemove = task;
+                break;
+            }
+        }
 
+        if (taskToRemove != null) {
+            todoItemTasks.remove(taskToRemove);
+        }
     }
 
     @Override
-    public void remove(Integer integer) {
-
-    }
-
-    @Override
-    public Collection<TodoItem> findAll() {
-        return null;
+    public Collection<TodoItemTask> findAll() {
+        return new ArrayList<>(todoItemTasks);
     }
 
     @Override
     public TodoItemTask findById(int id) {
+        for (TodoItemTask task : todoItemTasks) {
+            if (task.getId() == id) {
+                return task;
+            }
+        }
         return null;
     }
 
     @Override
     public Collection<TodoItemTask> findByAssignedStatus(boolean assigned) {
-        return null;
-    }
+        List<TodoItemTask> result = new ArrayList<>();
+        for (TodoItemTask task : todoItemTasks) {
+            if (task.isAssigned() == assigned) {
+                result.add(task);
+            }
+        }
+        return result;    }
 
     @Override
     public Collection<TodoItemTask> findByPersonId(int id) {
-        return null;
+        List<TodoItemTask> result = new ArrayList<>();
+        for (TodoItemTask task : todoItemTasks) {
+            if (task.getAssignee() != null && task.getAssignee().getId() == id) {
+                result.add(task);
+            }
+        }
+        return result;
     }
 }

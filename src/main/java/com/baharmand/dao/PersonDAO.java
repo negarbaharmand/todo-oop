1903bbfb8e5cd6dc4516fd9036aafc96fd2c3098
Negar Baharmand <negarbaharmand@outlook.com>
2023-08-30 15:52:02 +0200

    Implement PersonDAOCollection

--- src/main/java/com/baharmand/dao/PersonDAO.java
+++ src/main/java/com/baharmand/dao/PersonDAO.java
@@ -2,7 +2,7 @@ package com.baharmand.dao;
 
 import com.baharmand.model.Person;
 
-public interface PersonDAO extends BaseDAO<PersonDAO, Integer> {
+public interface PersonDAO extends BaseDAO<Person, Integer> {
     Person findById(int id);
     Person findByEmail(String email);
 }
--- src/main/java/com/baharmand/dao/PersonDAOCollection.java
+++ src/main/java/com/baharmand/dao/PersonDAOCollection.java
@@ -2,31 +2,59 @@ package com.baharmand.dao;
 
 import com.baharmand.model.Person;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
-public class PersonDAOCollection implements PersonDAO{
+public class PersonDAOCollection implements PersonDAO {
+    private Collection<Person> persons;
+
+    public PersonDAOCollection() {
+        persons = new ArrayList<>();
+    }
+
     @Override
-    public void persist(PersonDAO personDAO) {
+    public void persist(Person person) {
+        persons.add(person);
 
     }
 
     @Override
-    public void remove(Integer integer) {
+    public void remove(Integer id) {
+        Person personToRemove = null;
+        for (Person person : persons) {
+            if (person.getId() == id) {
+                personToRemove = person;
+                break;
+            }
+        }
 
+        if (personToRemove != null) {
+            persons.remove(personToRemove);
+        }
     }
 
     @Override
-    public Collection<PersonDAO> findAll() {
-        return null;
+    public Collection<Person> findAll() {
+        return new ArrayList<>(persons);
     }
 
     @Override
     public Person findById(int id) {
+        for (Person person : persons) {
+            if (person.getId() == id) {
+                return person;
+            }
+        }
         return null;
     }
 
     @Override
     public Person findByEmail(String email) {
+        for (Person person : persons) {
+            if (person.getEmail().equals(email)) {
+                return person;
+            }
+        }
         return null;
     }
 }
